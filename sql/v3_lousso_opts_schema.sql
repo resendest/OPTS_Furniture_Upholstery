4618fac67bd6b3a91bf2bdcc71403498a47c74a7
resendest <resendest@wit.edu>
2025-07-16 22:16:44 -0400

    up to date db schema

--- sql/v3_lousso_opts_schema.sql
+++ sql/v3_lousso_opts_schema.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 17.5
 -- Dumped by pg_dump version 17.5
 
--- Started on 2025-06-25 20:08:54
+
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -13,7 +13,8 @@ SET idle_in_transaction_session_timeout = 0;
 SET transaction_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
+-- (removed) SELECT pg_catalog.set_config('search_path', '', false);
+SET search_path = public;
 SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
@@ -34,7 +35,7 @@ END;
 $$;
 
 
-ALTER FUNCTION public.update_updated_at_column() OWNER TO postgres;
+-- ALTER FUNCTION public.update_updated_at_column() OWNER TO postgres;
 
 SET default_tablespace = '';
 
@@ -53,11 +54,15 @@ CREATE TABLE public.customers (
     address text,
     city text,
     state text,
-    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    register_token character varying(64),
+    password_hash text,
+    registered_at timestamp without time zone,
+    is_staff boolean DEFAULT false
 );
 
 
-ALTER TABLE public.customers OWNER TO postgres;
+-- ALTER TABLE public.customers OWNER TO postgres;
 
 --
 -- TOC entry 221 (class 1259 OID 19633)
@@ -73,10 +78,10 @@ CREATE SEQUENCE public.customers_customer_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE public.customers_customer_id_seq OWNER TO postgres;
+-- ALTER SEQUENCE public.customers_customer_id_seq OWNER TO postgres;
 
 --
--- TOC entry 5014 (class 0 OID 0)
+-- TOC entry 4997 (class 0 OID 0)
 -- Dependencies: 221
 -- Name: customers_customer_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -97,7 +102,7 @@ CREATE TABLE public.employees (
 );
 
 
-ALTER TABLE public.employees OWNER TO postgres;
+-- ALTER TABLE public.employees OWNER TO postgres;
 
 --
 -- TOC entry 227 (class 1259 OID 19661)
@@ -113,10 +118,10 @@ CREATE SEQUENCE public.employees_employee_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE public.employees_employee_id_seq OWNER TO postgres;
+-- ALTER SEQUENCE public.employees_employee_id_seq OWNER TO postgres;
 
 --
--- TOC entry 5015 (class 0 OID 0)
+-- TOC entry 4998 (class 0 OID 0)
 -- Dependencies: 227
 -- Name: employees_employee_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -138,7 +143,7 @@ CREATE TABLE public.item_workflow (
 );
 
 
-ALTER TABLE public.item_workflow OWNER TO postgres;
+-- ALTER TABLE public.item_workflow OWNER TO postgres;
 
 --
 -- TOC entry 233 (class 1259 OID 19706)
@@ -154,10 +159,10 @@ CREATE SEQUENCE public.item_workflow_workflow_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE public.item_workflow_workflow_id_seq OWNER TO postgres;
+-- ALTER SEQUENCE public.item_workflow_workflow_id_seq OWNER TO postgres;
 
 --
--- TOC entry 5016 (class 0 OID 0)
+-- TOC entry 4999 (class 0 OID 0)
 -- Dependencies: 233
 -- Name: item_workflow_workflow_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -181,7 +186,7 @@ CREATE TABLE public.order_items (
 );
 
 
-ALTER TABLE public.order_items OWNER TO postgres;
+-- ALTER TABLE public.order_items OWNER TO postgres;
 
 --
 -- TOC entry 231 (class 1259 OID 19684)
@@ -197,15 +202,15 @@ CREATE SEQUENCE public.order_items_item_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE public.order_items_item_id_seq OWNER TO postgres;
+-- ALTER SEQUENCE public.order_items_item_id_seq OWNER TO postgres;
 
 --
--- TOC entry 5017 (class 0 OID 0)
+-- TOC entry 5000 (class 0 OID 0)
 -- Dependencies: 231
 -- Name: order_items_item_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
-ALTER SEQUENCE public.order_items_item_id_seq OWNED BY public.order_items.item_id;
+-- ALTER SEQUENCE public.order_items_item_id_seq OWNED BY public.order_items.item_id;
 
 
 --
@@ -221,11 +226,12 @@ CREATE TABLE public.order_milestones (
     notes text,
     "timestamp" timestamp without time zone DEFAULT now(),
     is_client_action boolean DEFAULT false,
-    is_approved boolean DEFAULT false
+    is_approved boolean DEFAULT false,
+    status character varying(32) DEFAULT 'Not Started'::character varying
 );
 
 
-ALTER TABLE public.order_milestones OWNER TO postgres;
+-- ALTER TABLE public.order_milestones OWNER TO postgres;
 
 --
 -- TOC entry 237 (class 1259 OID 19751)
@@ -241,10 +247,10 @@ CREATE SEQUENCE public.order_milestones_milestone_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE public.order_milestones_milestone_id_seq OWNER TO postgres;
+-- ALTER SEQUENCE public.order_milestones_milestone_id_seq OWNER TO postgres;
 
 --
--- TOC entry 5018 (class 0 OID 0)
+-- TOC entry 5001 (class 0 OID 0)
 -- Dependencies: 237
 -- Name: order_milestones_milestone_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -279,7 +285,7 @@ CREATE TABLE public.order_specs (
 );
 
 
-ALTER TABLE public.order_specs OWNER TO postgres;
+-- ALTER TABLE public.order_specs OWNER TO postgres;
 
 --
 -- TOC entry 230 (class 1259 OID 19669)
@@ -303,7 +309,7 @@ CREATE TABLE public.orders (
 );
 
 
-ALTER TABLE public.orders OWNER TO postgres;
+-- ALTER TABLE public.orders OWNER TO postgres;
 
 --
 -- TOC entry 229 (class 1259 OID 19668)
@@ -319,10 +325,10 @@ CREATE SEQUENCE public.orders_order_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE public.orders_order_id_seq OWNER TO postgres;
+-- ALTER SEQUENCE public.orders_order_id_seq OWNER TO postgres;
 
 --
--- TOC entry 5019 (class 0 OID 0)
+-- TOC entry 5002 (class 0 OID 0)
 -- Dependencies: 229
 -- Name: orders_order_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -342,7 +348,7 @@ CREATE TABLE public.products (
 );
 
 
-ALTER TABLE public.products OWNER TO postgres;
+-- ALTER TABLE public.products OWNER TO postgres;
 
 --
 -- TOC entry 223 (class 1259 OID 19643)
@@ -358,10 +364,10 @@ CREATE SEQUENCE public.products_product_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE public.products_product_id_seq OWNER TO postgres;
+-- ALTER SEQUENCE public.products_product_id_seq OWNER TO postgres;
 
 --
--- TOC entry 5020 (class 0 OID 0)
+-- TOC entry 5003 (class 0 OID 0)
 -- Dependencies: 223
 -- Name: products_product_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -377,16 +383,16 @@ ALTER SEQUENCE public.products_product_id_seq OWNED BY public.products.product_i
 CREATE TABLE public.scan_events (
     scan_id integer NOT NULL,
     item_id integer,
-    task_id integer,
     employee_id integer,
     scan_time timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
     status character varying(20) NOT NULL,
     notes text,
+    milestone_id integer,
     CONSTRAINT scan_events_status_check CHECK (((status)::text = ANY ((ARRAY['Started'::character varying, 'Completed'::character varying])::text[])))
 );
 
 
-ALTER TABLE public.scan_events OWNER TO postgres;
+-- ALTER TABLE public.scan_events OWNER TO postgres;
 
 --
 -- TOC entry 235 (class 1259 OID 19725)
@@ -402,10 +408,10 @@ CREATE SEQUENCE public.scan_events_scan_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE public.scan_events_scan_id_seq OWNER TO postgres;
+-- ALTER SEQUENCE public.scan_events_scan_id_seq OWNER TO postgres;
 
 --
--- TOC entry 5021 (class 0 OID 0)
+-- TOC entry 5004 (class 0 OID 0)
 -- Dependencies: 235
 -- Name: scan_events_scan_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -427,7 +433,7 @@ CREATE TABLE public.tasks (
 );
 
 
-ALTER TABLE public.tasks OWNER TO postgres;
+-- ALTER TABLE public.tasks OWNER TO postgres;
 
 --
 -- TOC entry 225 (class 1259 OID 19652)
@@ -443,10 +449,10 @@ CREATE SEQUENCE public.tasks_task_id_seq
     CACHE 1;
 
 
-ALTER SEQUENCE public.tasks_task_id_seq OWNER TO postgres;
+-- ALTER SEQUENCE public.tasks_task_id_seq OWNER TO postgres;
 
 --
--- TOC entry 5022 (class 0 OID 0)
+-- TOC entry 5005 (class 0 OID 0)
 -- Dependencies: 225
 -- Name: tasks_task_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
@@ -463,7 +469,7 @@ ALTER TABLE ONLY public.customers ALTER COLUMN customer_id SET DEFAULT nextval('
 
 
 --
--- TOC entry 4795 (class 2604 OID 19665)
+-- TOC entry 4796 (class 2604 OID 19665)
 -- Name: employees employee_id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -471,7 +477,7 @@ ALTER TABLE ONLY public.employees ALTER COLUMN employee_id SET DEFAULT nextval('
 
 
 --
--- TOC entry 4804 (class 2604 OID 19710)
+-- TOC entry 4805 (class 2604 OID 19710)
 -- Name: item_workflow workflow_id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -479,7 +485,7 @@ ALTER TABLE ONLY public.item_workflow ALTER COLUMN workflow_id SET DEFAULT nextv
 
 
 --
--- TOC entry 4802 (class 2604 OID 19688)
+-- TOC entry 4803 (class 2604 OID 19688)
 -- Name: order_items item_id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -487,7 +493,7 @@ ALTER TABLE ONLY public.order_items ALTER COLUMN item_id SET DEFAULT nextval('pu
 
 
 --
--- TOC entry 4807 (class 2604 OID 19755)
+-- TOC entry 4808 (class 2604 OID 19755)
 -- Name: order_milestones milestone_id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -495,7 +501,7 @@ ALTER TABLE ONLY public.order_milestones ALTER COLUMN milestone_id SET DEFAULT n
 
 
 --
--- TOC entry 4796 (class 2604 OID 19672)
+-- TOC entry 4797 (class 2604 OID 19672)
 -- Name: orders order_id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -503,7 +509,7 @@ ALTER TABLE ONLY public.orders ALTER COLUMN order_id SET DEFAULT nextval('public
 
 
 --
--- TOC entry 4793 (class 2604 OID 19647)
+-- TOC entry 4794 (class 2604 OID 19647)
 -- Name: products product_id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -511,7 +517,7 @@ ALTER TABLE ONLY public.products ALTER COLUMN product_id SET DEFAULT nextval('pu
 
 
 --
--- TOC entry 4805 (class 2604 OID 19729)
+-- TOC entry 4806 (class 2604 OID 19729)
 -- Name: scan_events scan_id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -519,14 +525,210 @@ ALTER TABLE ONLY public.scan_events ALTER COLUMN scan_id SET DEFAULT nextval('pu
 
 
 --
--- TOC entry 4794 (class 2604 OID 19656)
+-- TOC entry 4795 (class 2604 OID 19656)
 -- Name: tasks task_id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.tasks ALTER COLUMN task_id SET DEFAULT nextval('public.tasks_task_id_seq'::regclass);
 
 
--- Completed on 2025-06-25 20:08:54
+--
+-- TOC entry 4815 (class 2606 OID 19642)
+-- Name: customers customers_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.customers
+    ADD CONSTRAINT customers_pkey PRIMARY KEY (customer_id);
+
+
+--
+-- TOC entry 4821 (class 2606 OID 19667)
+-- Name: employees employees_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.employees
+    ADD CONSTRAINT employees_pkey PRIMARY KEY (employee_id);
+
+
+--
+-- TOC entry 4829 (class 2606 OID 19714)
+-- Name: item_workflow item_workflow_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.item_workflow
+    ADD CONSTRAINT item_workflow_pkey PRIMARY KEY (workflow_id);
+
+
+--
+-- TOC entry 4825 (class 2606 OID 19695)
+-- Name: order_items order_items_barcode_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.order_items
+    ADD CONSTRAINT order_items_barcode_key UNIQUE (barcode);
+
+
+--
+-- TOC entry 4827 (class 2606 OID 19693)
+-- Name: order_items order_items_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.order_items
+    ADD CONSTRAINT order_items_pkey PRIMARY KEY (item_id);
+
+
+--
+-- TOC entry 4833 (class 2606 OID 19762)
+-- Name: order_milestones order_milestones_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.order_milestones
+    ADD CONSTRAINT order_milestones_pkey PRIMARY KEY (milestone_id);
+
+
+--
+-- TOC entry 4835 (class 2606 OID 19817)
+-- Name: order_specs order_specs_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.order_specs
+    ADD CONSTRAINT order_specs_pkey PRIMARY KEY (order_id);
+
+
+--
+-- TOC entry 4823 (class 2606 OID 19678)
+-- Name: orders orders_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT orders_pkey PRIMARY KEY (order_id);
+
+
+--
+-- TOC entry 4817 (class 2606 OID 19651)
+-- Name: products products_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.products
+    ADD CONSTRAINT products_pkey PRIMARY KEY (product_id);
+
+
+--
+-- TOC entry 4831 (class 2606 OID 19735)
+-- Name: scan_events scan_events_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.scan_events
+    ADD CONSTRAINT scan_events_pkey PRIMARY KEY (scan_id);
+
+
+--
+-- TOC entry 4819 (class 2606 OID 19660)
+-- Name: tasks tasks_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.tasks
+    ADD CONSTRAINT tasks_pkey PRIMARY KEY (task_id);
+
+
+--
+-- TOC entry 4846 (class 2620 OID 19773)
+-- Name: order_items trg_order_items_updated_at; Type: TRIGGER; Schema: public; Owner: postgres
+--
+
+CREATE TRIGGER trg_order_items_updated_at BEFORE UPDATE ON public.order_items FOR EACH ROW EXECUTE FUNCTION public.update_updated_at_column();
+
+
+--
+-- TOC entry 4845 (class 2620 OID 19772)
+-- Name: orders trg_orders_updated_at; Type: TRIGGER; Schema: public; Owner: postgres
+--
+
+CREATE TRIGGER trg_orders_updated_at BEFORE UPDATE ON public.orders FOR EACH ROW EXECUTE FUNCTION public.update_updated_at_column();
+
+
+--
+-- TOC entry 4838 (class 2606 OID 19715)
+-- Name: item_workflow item_workflow_item_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.item_workflow
+    ADD CONSTRAINT item_workflow_item_id_fkey FOREIGN KEY (item_id) REFERENCES public.order_items(item_id);
+
+
+--
+-- TOC entry 4839 (class 2606 OID 19720)
+-- Name: item_workflow item_workflow_task_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.item_workflow
+    ADD CONSTRAINT item_workflow_task_id_fkey FOREIGN KEY (task_id) REFERENCES public.tasks(task_id);
+
+
+--
+-- TOC entry 4837 (class 2606 OID 19696)
+-- Name: order_items order_items_order_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.order_items
+    ADD CONSTRAINT order_items_order_id_fkey FOREIGN KEY (order_id) REFERENCES public.orders(order_id);
+
+
+--
+-- TOC entry 4843 (class 2606 OID 19763)
+-- Name: order_milestones order_milestones_order_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.order_milestones
+    ADD CONSTRAINT order_milestones_order_id_fkey FOREIGN KEY (order_id) REFERENCES public.orders(order_id) ON DELETE CASCADE;
+
+
+--
+-- TOC entry 4844 (class 2606 OID 19818)
+-- Name: order_specs order_specs_order_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.order_specs
+    ADD CONSTRAINT order_specs_order_id_fkey FOREIGN KEY (order_id) REFERENCES public.orders(order_id);
+
+
+--
+-- TOC entry 4836 (class 2606 OID 19679)
+-- Name: orders orders_customer_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT orders_customer_id_fkey FOREIGN KEY (customer_id) REFERENCES public.customers(customer_id);
+
+
+--
+-- TOC entry 4840 (class 2606 OID 28040)
+-- Name: scan_events scan_events_employee_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.scan_events
+    ADD CONSTRAINT scan_events_employee_id_fkey FOREIGN KEY (employee_id) REFERENCES public.customers(customer_id);
+
+
+--
+-- TOC entry 4841 (class 2606 OID 19736)
+-- Name: scan_events scan_events_item_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.scan_events
+    ADD CONSTRAINT scan_events_item_id_fkey FOREIGN KEY (item_id) REFERENCES public.order_items(item_id);
+
+
+--
+-- TOC entry 4842 (class 2606 OID 28035)
+-- Name: scan_events scan_events_milestone_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.scan_events
+    ADD CONSTRAINT scan_events_milestone_id_fkey FOREIGN KEY (milestone_id) REFERENCES public.order_milestones(milestone_id);
+
+
+-- Completed on 2025-07-16 22:07:26
 
 --
 -- PostgreSQL database dump complete
